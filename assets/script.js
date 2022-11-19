821aeed71029a09a74de2196166a8ac724d6be55
sdanimc <116037382+sdanimc@users.noreply.github.com>
2022-11-19 12:33:18 -0800

    outlined js logic in comments and added base functions and get user input functionality

---
+++ assets/script.js
@@ -0,0 +1,21 @@
+var userInput;
+var searchBtn = document.getElementById('searchBtn')
+function getUserInput() {
+userInput = document.getElementById('input').value;
+//search();
+};
+function search(){
+    /*
+    //run api
+    //if user input invalid {alert ('We don't have this city in our database 
+    //Please try again'); return search;} else{ add to search history and run display function
+    }*/
+}
+function displayData(){
+    /* get current and next 5 dates
+function? display current date and conditions
+for each fivedayfore display date temp wind and display*/
+}
+//event listener for search history btns with function that sets user input to btn text and runs search
+//event listener for search btn that runs function get input
+searchBtn.addEventListener('click', getUserInput);
