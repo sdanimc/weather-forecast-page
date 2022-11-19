02aafe83ee04c648ae6ff5350fdcf6ceb27d0ef4
sdanimc <116037382+sdanimc@users.noreply.github.com>
2022-11-19 13:45:38 -0800

    added js functionality to get coordinates and weather from APIs, changed header color and created html elements for data display

--- assets/script.js
+++ assets/script.js
@@ -1,21 +1,49 @@
+//globally declared variables
 var userInput;
 var searchBtn = document.getElementById('searchBtn')
+//apikeyw can be used for both geocoding and getting weather info
+var APIkeyW = "e62d64be9b5b8baedff8301f0d55e47f";
+
+//functions
 function getUserInput() {
-userInput = document.getElementById('input').value;
-//search();
+    userInput = document.getElementById('input').value;
+    console.log(userInput);
+    search();
 };
-function search(){
-    /*
-    //run api
-    //if user input invalid {alert ('We don't have this city in our database 
-    //Please try again'); return search;} else{ add to search history and run display function
-    }*/
+function search() {
+    //convert userInput to lat and lon with geocoding api
+    var requestURLgeo = 'http://api.openweathermap.org/geo/1.0/direct?q=' + userInput + '&limit=1&appid=' + APIkeyW;
+    fetch(requestURLgeo)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+            if (!data[0]) { alert('This city is not in our database. Please try again') };
+            var lat = data[0].lat;
+            var lon = data[0].lon;
+            getWeather(lat, lon);
+        });
+    function getWeather(lat, lon) {
+        var requestURLwet = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + APIkeyW;
+        fetch(requestURLwet)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);});
+        /*
+         add to search history and run display function
+         may have to run display data inside this function to reference data
+        */
+    };
 }
-function displayData(){
+function displayData() {
     /* get current and next 5 dates
 function? display current date and conditions
 for each fivedayfore display date temp wind and display*/
 }
+
+//event listeners
 //event listener for search history btns with function that sets user input to btn text and runs search
-//event listener for search btn that runs function get input
 searchBtn.addEventListener('click', getUserInput);
